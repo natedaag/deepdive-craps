d128d9551b5ec780a117896faf9fe641f902ca69
Nathan <natedaage@yahoo.com>
2017-07-10 13:47:10 -0600

    added photos of dice and roll

--- src/edu/cnm/deepdive/craps/ConsoleGame.java
+++ src/edu/cnm/deepdive/craps/ConsoleGame.java
@@ -24,12 +24,12 @@ public class ConsoleGame {
     croupier.setPlayable((wins, losses) -> {
       String input = System.console().readLine(
           "%d wins, %d losses. Play again? (y/n)", wins, losses);
-      return (input.toLowerCase().charAt(0) == 'y');      
+      return (input.length() == 0 || input.toLowerCase().charAt(0) == 'y');      
     });
     croupier.setContinuable((wins, losses, point) -> {
       String input = System.console().readLine(
-          "Point is %d, Continue play? (y/n)", wins, losses);
-      return (input.toLowerCase().charAt(0) == 'y');            
+          "Point is %d, Continue play? (y/n)", point);
+      return (input.length() == 0 || input.toLowerCase().charAt(0) == 'y');            
     });
     croupier.play();
   }
